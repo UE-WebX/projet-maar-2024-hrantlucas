5bf655597844d0c48f3112f6ff37efcf62aedd8f
lucasbihannic <lucas.bihannic@master-developpement-logiciel.fr>
2024-02-11 18:06:37 +0100

    add-tests

--- src/test/java/org/hrantlucas/RecipeEndpointTest.java
+++ src/test/java/org/hrantlucas/RecipeEndpointTest.java
@@ -4,12 +4,14 @@ import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
 import jakarta.ws.rs.client.WebTarget;
 
+import jakarta.ws.rs.core.Response;
 import org.glassfish.grizzly.http.server.HttpServer;
 
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 public class RecipeEndpointTest {
 
@@ -37,12 +39,32 @@ public class RecipeEndpointTest {
         server.stop();
     }
 
-    /**
-     * Test to see that the message "Got it!" is sent in the response.
-     */
     @Test
-    public void testGetIt() {
-        String responseMsg = target.path("myresource").request().get(String.class);
-        assertEquals("Got it!", responseMsg);
+    public void testGetRecipeByCuisineTypeSuccess() {
+        Response response = target.path("/recipe/meal/Asian").request().get();
+        assertEquals( 200, response.getStatus(), "the response doesn't have a 200 http status code");
+
+        String responseBody = response.readEntity(String.class);
+        assertNotNull(responseBody, "the response doesn't have a body");
+        assertTrue(responseBody.contains("<cuisineType>asian</cuisineType>"), "the response doesn't contain the good cuisine type");
+    }
+
+    @Test
+    public void testGetRecipeByCuisineTypeInvalid() {
+        Response response = target.path("/recipe/meal/InvalidCuisine").request().get();
+        assertEquals(400, response.getStatus(), "the response doesn't have a 400 http status code");
+
+        String responseBody = response.readEntity(String.class);
+        assertNotNull(responseBody, "the response doesn't have a body");
+        assertTrue(responseBody.contains("<errorCode>400</errorCode>"), "the response doesn't contain the error message");
+    }
+
+    @Test
+    public void testGetRecipeByCuisineTypeWithServerError() {
+        Response response = target.path("/recipe/InvalidURL").request().get();
+        assertEquals(500, response.getStatus(), "the response doesn't have a 500 http status code");
+
+        String responseBody = response.readEntity(String.class);
+        assertTrue(responseBody.contains("<errorCode>500</errorCode>"), "the response doesn't contain the error message");
     }
 }
